cfda07b0d654839133abdb34a5c27eaa0741c8ea
Justin <justindado@yahoo.com>
2016-11-06 19:51:09 +1300

    added react to entry point js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {render} from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
 
 import App from './components/App'
 
 document.addEventListener.('DOMContentLoaded', () => {
   ReactDOM.render(
-    App/
+    <Router history={hashHistory}>
+      <Route path='/' component={App}>
+    </Router>
   )
 })
