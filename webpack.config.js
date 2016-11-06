cf539305571c4b2c4cdeec4b94e40805fe9ee099
Justin <justindado@yahoo.com>
2016-11-06 19:35:02 +1300

    installed webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+import path from 'path'
+
+module.exports = {
+  entry: './src/index.js',
+  output: 'bundle.js',
+  module: {
+    loaders: [
+      {loader: 'babel-loader', test: /\.jsx?$/, exclude: /node_modules/}
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devtool: 'source-map'
+}
