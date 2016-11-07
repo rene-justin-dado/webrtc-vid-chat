f7f02f1fe5117226f89e0d2fb6fe961b67f98799
Justin <justindado@yahoo.com>
2016-11-07 20:31:48 +1300

    properly rendering app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import LocalVideo from './LocalVideo'
 
 export default React.createClass({
   render () {
     return (
       <div className="app">
-        <h1>JUMP</h1>
         <LocalVideo />
       </div>
     )
--- src/components/LocalVideo.jsx
+++ src/components/LocalVideo.jsx
@@ -5,8 +5,8 @@ export default React.createClass({
     return (
       <div id="local-video-container">
         <h1>WebRTC Video</h1>
-        <video id="localVideo" autoplay>local</video>
-        <video id="remoteVideo" autoplay>remote</video>
+        <video id="localVideo" autoPlay>local</video>
+        <video id="remoteVideo" autoPlay>remote</video>
         <div id="local-video-controls">
           <button id="startButton">Start</button>
           <button id="callButton">Call</button>
