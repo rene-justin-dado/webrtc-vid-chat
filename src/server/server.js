fd6be3b4d60c86afdc02a8569bd3f34be528a1b9
Justin <justindado@yahoo.com>
2016-11-07 21:55:40 +1300

    initialised express server with sockets

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const cors = require('cors')
+import express from 'express'
+import cors from 'cors'
+import {Server} from 'http'
 
 const app = express()
+const http = Server(app)
+const io = require('socket.io')(http)
 const PORT = process.env.PORT || 5000
 
 app.use(express.static(__dirname + './../public'))
 app.use(cors())
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+http.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
