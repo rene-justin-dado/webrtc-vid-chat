e7986e6e13fec8b027c9e5c469ebc6bf3cec195b
Justin <justindado@yahoo.com>
2016-11-11 11:11:42 +1300

    forewent import statements in server

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,18 @@
-import express from 'express'
-import cors from 'cors'
-import {Server} from 'http'
+const express = require('express')
+const app = require('express')()
+const cors = require('cors')
+const Server = require('http').Server
 
-const app = express()
 const http = Server(app)
 const io = require('socket.io')(http)
 const PORT = process.env.PORT || 5000
+
+io.on('connection', socket => {
+  console.log('a user connected')
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg)
+  })
+})
 
 app.use(express.static(__dirname + './../public'))
 app.use(cors())
