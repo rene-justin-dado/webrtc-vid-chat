e94675603ebff66aee045e62c78d63e3c78539e1
Justin <justindado@yahoo.com>
2016-11-11 13:05:32 +1300

    updated webpack config with relative entry path

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  entry: './src/index.js',
+  entry: path.join(__dirname, 'src'),
   output: {
     path: path.join(__dirname, 'src', 'public'),
     filename: 'bundle.js'
