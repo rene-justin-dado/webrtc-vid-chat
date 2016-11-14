86ab324d851ab77793d51ce91a8e996f2c1a2ecb
Justin <justindado@yahoo.com>
2016-11-14 20:50:43 +1300

    changed entry to string

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  entry: path.join(__dirname, 'src'),
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'src', 'public'),
     filename: 'bundle.js'
